40c4c30036aa73ec90c03460da42da0ac388c12c
Nacho Movellan <nmovellan2@hotmail.com>
2018-07-02 19:29:09 +0200

    Init

---
+++ XamFilm/XamFilm/Views/MainViewMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamFilm.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainViewMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MainViewMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainViewMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MainViewMenuItem> MenuItems { get; set; }
+            
+            public MainViewMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MainViewMenuItem>(new[]
+                {
+                    new MainViewMenuItem { Id = 0, Title = "Page 1" },
+                    new MainViewMenuItem { Id = 1, Title = "Page 2" },
+                    new MainViewMenuItem { Id = 2, Title = "Page 3" },
+                    new MainViewMenuItem { Id = 3, Title = "Page 4" },
+                    new MainViewMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ XamFilm/XamFilm/Views/MainViewMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamFilm.Views
+{
+
+    public class MainViewMenuItem
+    {
+        public MainViewMenuItem()
+        {
+            TargetType = typeof(MainViewDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
