8c569be08d256f0177a6879b0aaa79f1592b3727
Nacho Movellan <nmovellan2@hotmail.com>
2018-07-02 19:26:06 +0200

    Agregar archivos de proyecto.

---
+++ XamFilm/XamFilm/App.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace XamFilm
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+            LiveReload.Init();//Para poder utilizar Live Reload
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
